bb63d5faf8275875512ed3d06e64bd0919640b57
tienouc <willtien@outlook.com>
2019-09-14 23:01:37 +0800

    fix url err

--- js/_Layout/waterFall.js
+++ js/_Layout/waterFall.js
@@ -1,57 +1,57 @@
 var data = [
     {
-      "src": "1.png",
+      "src": "../img/_Layout-img/1.png",
       "title": "第一怪"
     },{
-      "src": "2.png",
+      "src": "../img/_Layout-img/2.png",
       "title": "第二怪"
     },{
-      "src": "3.png",
+      "src": "../img/_Layout-img/3.png",
       "title": "第三怪"
     },{
-      "src": "4.png",
+      "src": "../img/_Layout-img/4.png",
       "title": "第四怪"
     },{
-      "src": "5.png",
+      "src": "../img/_Layout-img/5.png",
       "title": "第五怪"
     },{
-      "src": "6.png",
+      "src": "../img/_Layout-img/6.png",
       "title": "第六怪"
     },{
-      "src": "7.png",
+      "src": "../img/_Layout-img/7.png",
       "title": "第七怪"
     },{
-      "src": "8.png",
+      "src": "../img/_Layout-img/8.png",
       "title": "第八怪"
     },{
-      "src": "9.png",
+      "src": "../img/_Layout-img/9.png",
       "title": "第九怪"
     },{
-      "src": "10.png",
+      "src": "../img/_Layout-img/10.png",
       "title": "第十怪"
     },{
-      "src": "11.png",
+      "src": "../img/_Layout-img/11.png",
       "title": "第十一怪"
     },{
-      "src": "12.png",
+      "src": "../img/_Layout-img/12.png",
       "title": "第十二怪"
     },{
-      "src": "13.png",
+      "src": "../img/_Layout-img/13.png",
       "title": "第十三怪"
     },{
-      "src": "14.png",
+      "src": "../img/_Layout-img/14.png",
       "title": "第十四怪"
     },{
-      "src": "15.png",
+      "src": "../img/_Layout-img/15.png",
       "title": "第十五怪"
     },{
-      "src": "16.png",
+      "src": "../img/_Layout-img/16.png",
       "title": "第十六怪"
     },{
-      "src": "17.png",
+      "src": "../img/_Layout-img/17.png",
       "title": "第十七怪"
     },{
-      "src": "18.png",
+      "src": "../img/_Layout-img/18.png",
       "title": "第十八怪"
     }
   ]
